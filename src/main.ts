74bc641a2d1790bda7800dfacdbd12442633dd19
angurgapi <sophiekovalsky@gmail.com>
2023-01-21 23:07:59 +0400

    port in main

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
+  console.log(process.env)
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
